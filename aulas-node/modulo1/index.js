2e5e86f30e9b3f2b5582dd3faf4e099ac56c2c55
Rodrigo Dias <diasbass@hotmail.com>
2019-02-18 15:09:53 -0300

    Modulo 1 - 9

--- aulas-node/modulo1/index.js
+++ aulas-node/modulo1/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
 
+nunjucks.configure("views", {
+  autoscape: true,
+  express: app,
+  watch: true
+});
+
+app.set("view engine", "njk");
+
+const users = ["Usuario A", "Usuario B", "Usuario C"];
+
 app.get("/", (req, res) => {
-  return res.send(`Bem-vindo`);
+  return res.render("list", { users });
+});
+
+app.get("/new", (req, res) => {
+  return res.render("new");
 });
 
 app.listen(3000);
